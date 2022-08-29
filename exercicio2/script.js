8f03c044922471f8246fcaed21c53ff5c25cd8bc
gustavojb1 <gustavojunqueirab@gmail.com>
2022-08-29 15:24:32 -0300

    alteração ex 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -8,12 +8,12 @@ console.log(resposta2)
 console.log(resposta3)
 */
 
-pergunta1 = "Você ja fez faculdade?"
-resposta1 = prompt(pergunta1)
-pergunta2 = "Você ja trabalhou de carteira assinada?"
-resposta2 = prompt(pergunta2)
-pergunta3 = "Você sabe falar Inglês?"
-resposta3 = prompt(pergunta3)
+let pergunta1 = "Você ja fez faculdade?"
+let resposta1 = prompt(pergunta1)
+let pergunta2 = "Você ja trabalhou de carteira assinada?"
+let resposta2 = prompt(pergunta2)
+let pergunta3 = "Você sabe falar Inglês?"
+let resposta3 = prompt(pergunta3)
 
 console.log(pergunta1, resposta1)
 console.log(pergunta2, resposta2)
